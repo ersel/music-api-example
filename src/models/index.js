2d059b3e9353b96fbd84e64fd3de78ffccd519f3
buseozkan <buse1.ozkan@gmail.com>
2021-03-08 12:03:09 +0200

    creating artist started

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize');
+const ArtistModel = require('./artist');
 const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+
 
 const setupDatabase = () => {
     const connection = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
@@ -8,9 +10,13 @@ const setupDatabase = () => {
         dialect: 'mysql',
         logging: false
     });
+    
+    const Artist = ArtistModel(connection, Sequelize);
 
     connection.sync({ alter: true });
-    return{};
+    return{
+        Artist
+    };
 };
 
 module.exports = setupDatabase();
