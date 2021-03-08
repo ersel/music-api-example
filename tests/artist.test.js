e8c214376b0c665f2dbf81ec5d330a5abcd1637e
buseozkan <buse1.ozkan@gmail.com>
2021-03-08 20:38:36 +0200

    test

--- tests/artist.test.js
+++ tests/artist.test.js
@@ -1,7 +1,8 @@
+/*
 const { expect } = require('chai');
 const request = require('supertest');
 
-/* artists.test.js */ 
+artists.test.js 
 const { Artist } = require('../src/models');
 const app = require('../src/app');
 
@@ -25,8 +26,44 @@ describe('POST /artists', (done) => {
             name: 'Tame Impala',
             genre: 'Rock',
         }).then(response => {
+            console.log(response.status);
             expect(response.status).to.equal(201);
             done();
         }).catch(error => done(error));
     });
 });
+*/
+/* tests/artists.test.js */
+const { expect } = require('chai');
+const request = require('supertest');
+const { Artist } = require('../src/models');
+const app = require('../src/app');
+
+describe('/artists', () => {
+    before(done => {
+        Artist.sequelize
+            .sync()
+            .then(() => done())
+            .catch(error => done(error));
+    })
+
+    beforeEach(done => {
+        Artist.destroy({ where: {} })
+            .then(() => done()).catch(error => done(error));
+    })
+
+    describe('POST /artists', () => {
+        it('creates a new artist in the database', (done) => {
+            request(app).post('/artists').send({
+                name: 'Tame Impala',
+                genre: 'Rock',
+            }).then(response => {
+                console.log(response.status);
+                expect(response.status).to.equal(201);
+                done();
+            }).catch(error => done(error));
+        });
+    });
+});
+
+
